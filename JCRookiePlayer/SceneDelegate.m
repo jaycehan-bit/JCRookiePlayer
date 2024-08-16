4cb5ef00ea71b7a428d325a9e8183da6736d93ed
jaycehan <jaycehan@tencent.com>
2024-08-16 17:28:09 +0800

    删除无用代码

--- JCRookiePlayer/SceneDelegate.m
+++ JCRookiePlayer/SceneDelegate.m
@@ -6,7 +6,7 @@
 //
 
 #import "SceneDelegate.h"
-#import "JCRookiePlayer-Swift.h"
+#import "ViewController.h"
 
 @interface SceneDelegate ()
 
@@ -16,7 +16,7 @@
 
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
-    UIViewController *controller = [[JCViewController alloc] initWithNibName:nil bundle:nil];
+    UIViewController *controller = [[ViewController alloc] initWithNibName:nil bundle:nil];
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:controller];
     self.window.rootViewController = navigationController;
     [self.window makeKeyAndVisible];
--- JCRookiePlayer/ViewController.m
+++ JCRookiePlayer/ViewController.m
@@ -7,7 +7,14 @@
 
 #import "ViewController.h"
 
+static const CGFloat gButtonHeight = 45;
+static const CGFloat gButtonWidth = 200;
+
 @interface ViewController ()
+
+@property (nonatomic, strong) UIButton *playButton;
+
+@property (nonatomic, strong) UIButton *recordButton;
 
 @end
 
@@ -15,8 +22,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = UIColor.blackColor;
+    [self.view addSubview:self.playButton];
+    [self.view addSubview:self.recordButton];
 }
 
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.playButton.frame = CGRectMake((self.view.bounds.size.width - gButtonWidth) / 2.0, 150, gButtonWidth, gButtonHeight);
+    self.playButton.layer.cornerRadius = 3.5;
+    self.recordButton.frame = CGRectMake((self.view.bounds.size.width - gButtonWidth) / 2.0, 250, gButtonWidth, gButtonHeight);
+    self.recordButton.layer.cornerRadius = 3.5;
+}
+
+- (UIButton *)playButton {
+    if (!_playButton) {
+        _playButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        _playButton.backgroundColor = UIColor.systemBlueColor;
+        _playButton.titleLabel.font = [UIFont fontWithName:@"PingFangSC-Medium" size:14];
+        _playButton.titleLabel.text= @"播放";
+        _playButton.titleLabel.textColor = UIColor.whiteColor;
+    }
+    return _playButton;
+}
+
+- (UIButton *)recordButton {
+    if (!_recordButton) {
+        _recordButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        _recordButton.backgroundColor = UIColor.systemGreenColor;
+        _recordButton.titleLabel.font = [UIFont fontWithName:@"PingFangSC-Medium" size:14];
+        _recordButton.titleLabel.text= @"录制";
+        _recordButton.titleLabel.textColor = UIColor.whiteColor;
+    }
+    return _recordButton;
+}
 
 @end
